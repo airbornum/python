de0f07830b4802c1233376be62f192a92cefc4ce
Yevhen Khadzhamov <airbornum@gmail.com>
2019-06-03 15:36:34 +0300

    add lesson2

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+print ('Print your name')
+#Input function from keyboard
+MyName = input ()
+print( 'Your name maybe is ' + MyName)
+
+#String len
+print (len(MyName))
+
+MyAge = len (MyName)
+
+Use variable innteger in string
+print ('Your age is  '  + str(int(MyAge)))
+
